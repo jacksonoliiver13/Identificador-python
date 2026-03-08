2050de66d9f6dfbd833fd29defd2bbf6ac4fae1a
jacksonoliiver13 <jacksonoliiver13@gmail.com>
2026-03-08 15:07:00 -0300

    first commit

---
+++ Identificador.py
@@ -0,0 +1,18 @@
+# Programa que identifica quem é a pessoa
+
+Nome = ('Marquinhos')
+
+if Nome == 'Vitória Rosa':
+    print('Ela é o amor da sua vida')
+
+elif Nome == 'Maria Libencia':
+    print('Ela é sua mãe')
+
+elif Nome == 'Mayla':
+    print('Ela é sua filhota')
+
+elif Nome == 'Ana Julia':
+    print('Ela é sua sobrinha')
+
+else:
+    print('Quem é essa?')
